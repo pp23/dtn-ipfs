901ac48d5455d1999212f6854599fb4d7db9fcb0
Paul Peschel <paul.peschel@posteo.de>
2023-08-05 22:17:34 +0200

    Add peering test code
    NodeB connects to NodeA

--- cmd/dtn-ipfs/main.go
+++ cmd/dtn-ipfs/main.go
@@ -6,8 +6,11 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"sync"
 
 	icore "github.com/ipfs/boxo/coreiface"
+	"github.com/ipfs/boxo/files"
+	ma "github.com/multiformats/go-multiaddr"
 
 	"github.com/ipfs/kubo/config"
 	"github.com/ipfs/kubo/core"
@@ -15,6 +18,7 @@ import (
 	"github.com/ipfs/kubo/core/node/libp2p"
 	"github.com/ipfs/kubo/plugin/loader"
 	"github.com/ipfs/kubo/repo/fsrepo"
+	"github.com/libp2p/go-libp2p/core/peer"
 )
 
 func setupPlugins(externalPluginsPath string) error {
@@ -50,26 +54,36 @@ func createTempRepo() (string, error) {
 	return repoPath, nil
 }
 
-func createNode(ctx context.Context, repoPath string) (*core.IpfsNode, error) {
+func createNode(ctx context.Context, repoPath string, online bool) (*core.IpfsNode, error) {
 	repo, err := fsrepo.Open(repoPath)
 	if err != nil {
 		return nil, err
 	}
 	nodeOptions := &core.BuildCfg{
-		Online:  true,
+		Online:  online,
 		Routing: libp2p.DHTOption,
 		Repo:    repo,
 	}
 	return core.NewNode(ctx, nodeOptions)
 }
 
-func spawnEphemeral(ctx context.Context) (icore.CoreAPI, *core.IpfsNode, error) {
+var loadPluginsOnce sync.Once
+
+func spawnEphemeral(ctx context.Context, online bool) (icore.CoreAPI, *core.IpfsNode, error) {
+	var onceErr error
+	loadPluginsOnce.Do(func() {
+		onceErr = setupPlugins("")
+	})
+	if onceErr != nil {
+		return nil, nil, onceErr
+	}
+
 	repoPath, err := createTempRepo()
 	if err != nil {
 		return nil, nil, err
 	}
 
-	node, err := createNode(ctx, repoPath)
+	node, err := createNode(ctx, repoPath, online)
 	if err != nil {
 		return nil, nil, err
 	}
@@ -78,18 +92,79 @@ func spawnEphemeral(ctx context.Context) (icore.CoreAPI, *core.IpfsNode, error) 
 	return api, node, err
 }
 
+func connectToPeers(ctx context.Context, ipfs icore.CoreAPI, peers []string) error {
+	var wg sync.WaitGroup
+	peerInfos := make(map[peer.ID]*peer.AddrInfo, len(peers))
+	for _, addrStr := range peers {
+		addr, err := ma.NewMultiaddr(addrStr)
+		if err != nil {
+			return err
+		}
+		pii, err := peer.AddrInfoFromP2pAddr(addr)
+		if err != nil {
+			return err
+		}
+		pi, ok := peerInfos[pii.ID]
+		if !ok {
+			pi = &peer.AddrInfo{ID: pii.ID}
+			peerInfos[pi.ID] = pi
+		}
+		pi.Addrs = append(pi.Addrs, pii.Addrs...)
+	}
+
+	wg.Add(len(peerInfos))
+	for _, peerInfo := range peerInfos {
+		go func(peerInfo *peer.AddrInfo) {
+			defer wg.Done()
+			err := ipfs.Swarm().Connect(ctx, *peerInfo)
+			if err != nil {
+				log.Print("Failed to connecto to", peerInfo.ID, ": ", err)
+				return
+			}
+			log.Print("Connected to ", peerInfo.ID)
+		}(peerInfo)
+	}
+	wg.Wait()
+	return nil
+}
+
 func main() {
 	log.Print("Getting an IPFS node running")
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	ipfsA, nodeA, err := spawnEphemeral(ctx)
+	ipfsA, nodeA, err := spawnEphemeral(ctx, true)
 	if err != nil {
 		log.Fatal(err)
 	}
 	log.Print(nodeA.Identity)
-	log.Print(ipfsA.Name())
+
+	rootPeerCidFile, err := ipfsA.Unixfs().Add(ctx, files.NewBytesFile([]byte("rootPeer data")))
+	if err != nil {
+		log.Panic(err)
+	}
+
+	log.Print("Added rootPeer file with CID ", rootPeerCidFile.String())
+
+	ipfsB, _, err := spawnEphemeral(ctx, true)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	rootPeerMa := "/ip4/127.0.0.1/tcp/4001/p2p/" + nodeA.Identity.String()
+
+	bootstrapNodes := []string{
+		rootPeerMa,
+	}
+
+	go func() {
+		err := connectToPeers(ctx, ipfsB, bootstrapNodes)
+		if err != nil {
+			log.Print("failed connecto to peers: ", err)
+		}
+	}()
+
 	var wait chan struct{}
 	<-wait
 }
